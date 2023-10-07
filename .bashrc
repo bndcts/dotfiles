9688cee80dd44c25a49763cfdbb207c9654e8939
Benedikt.schwankner <benedikt.schwankner@tum.de>
2023-10-07 15:05:54 +0200

    setupscript

---
+++ .bashrc
@@ -0,0 +1,5 @@
+alias ls='ls --color=auto -ACF'
+alias ll='ls --color=auto -alF'
+alias grep='grep --color=auto'
+alias mkdir='mkdir -pv'
+alias mv='mv -v'
---
+++ scripts/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./symlink.sh
+./aptinstall.sh
+
+# Get all upgrades
+#apt upgrade -y
+
+
+# Fun hello
+#figlet "... and we're back!" | lolcat
